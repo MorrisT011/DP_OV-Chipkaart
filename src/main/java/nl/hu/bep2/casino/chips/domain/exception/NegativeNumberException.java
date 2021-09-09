08e8c8ad701cfd3dc2919e908bfebcfc59c4bf0e
MorrisT011 <74653965+MorrisT011@users.noreply.github.com>
2021-09-09 18:59:38 +0200

    test

--- src/main/java/nl/hu/bep2/casino/chips/domain/exception/NegativeNumberException.java
+++
@@ -1,7 +0,0 @@
-package nl.hu.bep2.casino.chips.domain.exception;
-
-public class NegativeNumberException extends RuntimeException {
-    public NegativeNumberException(String message) {
-        super(message);
-    }
-}
--- src/main/java/nl/hu/bep2/casino/chips/domain/exception/NotEnoughChipsException.java
+++
@@ -1,7 +0,0 @@
-package nl.hu.bep2.casino.chips.domain.exception;
-
-public class NotEnoughChipsException extends RuntimeException {
-    public NotEnoughChipsException(String message) {
-        super(message);
-    }
-}
